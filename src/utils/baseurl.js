10f71c03bb22672b1804c2d400ba9bd28b678c4c
shreyakush03 <shreyakush013@gmail.com>
2026-09-17 01:58:14 +0530

    URLL

--- src/utils/baseurl.js
+++ src/utils/baseurl.js
@@ -1,4 +1,4 @@
 export const BASE_URL = (
-  import.meta.env.VITE_API_URL ||
+  import.meta.env.BACKEND_API_URL ||
   (location.hostname === "localhost" ? "http://localhost:7777" : "/api")
 ).replace(/\/+$/, "");
