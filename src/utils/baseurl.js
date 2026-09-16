40c88c3500e3592964ca33f656caa1cdde947ef7
shreyakush03 <shreyakush013@gmail.com>
2026-09-17 01:15:59 +0530

    fixing backend for deployement

--- src/utils/baseurl.js
+++ src/utils/baseurl.js
@@ -1,1 +1,4 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api"
+export const BASE_URL = (
+  import.meta.env.VITE_API_URL ||
+  (location.hostname === "localhost" ? "http://localhost:7777" : "/api")
+).replace(/\/+$/, "");
